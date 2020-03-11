ab55b2dd736437b4a306a1137216f587c6fecb09
janimaenpaa <itsmayd@gmail.com>
2020-03-11 18:47:13 +0200

    Done 1.12 - 1.14

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
+  const [mostVoted, setMostVoted] = useState(0);
+
+  const newAnecdote = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomNumber);
+  };
+
+  const handleVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+
+    if (copy[mostVoted] < copy[selected]) {
+      setMostVoted(selected);
+    }
+  };
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <div>
+        has {votes[selected]} votes
+        <div>
+          <button onClick={handleVote}>vote</button>
+          <button onClick={newAnecdote}>next anecdote</button>
+        </div>
+        <div>
+          <h2>Anecdote with most votes</h2>
+          <p>{anecdotes[mostVoted]}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
