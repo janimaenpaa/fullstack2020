efd6f438157804cb4ac3ad7935879c142c585a6e
janimaenpaa <itsmayd@gmail.com>
2020-03-16 15:19:03 +0200

    2.19 - 2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,12 +4,14 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import personService from "./services/persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then(initialPersons => {
@@ -17,78 +19,32 @@ const App = () => {
     });
   }, []);
 
-  const addPerson = event => {
-    event.preventDefault();
-
-    const nameObject = {
-      name: newName,
-      number: newNumber
-    };
-
-    let id;
-
-    if (persons.find(person => person.name === newName)) {
-      id = persons.findIndex(person => person.name === newName) + 1;
-      if (
-        window.confirm(
-          `${newName} is already added to phonebook, replace the old number with a new one?`
-        )
-      ) {
-        return personService.update(id, nameObject).then(updatedPerson => {
-          setPersons(
-            persons.map(person =>
-              person.name === newName ? { ...updatedPerson } : person
-            )
-          );
-          alert("Number has been updated.");
-        });
-      }
-    }
-
-    personService.create(nameObject).then(returnedPerson => {
-      setPersons(persons.concat(returnedPerson));
-      setNewName("");
-      setNewNumber("");
-    });
-  };
-
   const deletePerson = person => {
     if (window.confirm(`Delete ${person.name} ?`)) {
       personService.remove(person.id).then(removedPerson => {
         setPersons(persons.filter(per => per.id !== person.id));
       });
+      setErrorMessage(`${person.name} deleted`);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
     }
-  };
-
-  const handleNameChange = event => {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = event => {
-    console.log(event.target.value);
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterChange = event => {
-    setFilter(event.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter
-        text="filter shown with"
-        value={filter}
-        handleFilterChange={handleFilterChange}
-      />
+      <Notification message={errorMessage} />
+      <Filter text="filter shown with" value={filter} setFilter={setFilter} />
       <h3>Add a new</h3>
       <PersonForm
-        addPerson={addPerson}
         newName={newName}
         newNumber={newNumber}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
+        persons={persons}
+        setPersons={setPersons}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
+        setErrorMessage={setErrorMessage}
       />
       <h3>Numbers</h3>
       <Persons filter={filter} persons={persons} deletePerson={deletePerson} />
--- osa2/puhelinluettelo/src/components/Filter.js
+++ osa2/puhelinluettelo/src/components/Filter.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Filter = ({ text, value, handleFilterChange }) => {
+const Filter = ({ text, value, setFilter }) => {
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
   return (
     <div>
       {text} <input value={value} onChange={handleFilterChange} />
