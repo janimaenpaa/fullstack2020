19e4cc2b9fb5ad2d071ed5637a6df667fc42e3ce
janimaenpaa <itsmayd@gmail.com>
2020-04-03 00:25:32 +0300

    Edit 4.8

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -32,6 +32,13 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/)
+})
+
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs")
 
