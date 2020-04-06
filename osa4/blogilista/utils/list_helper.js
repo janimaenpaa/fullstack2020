ec6c64026e5f61c7cb60ad58c7c8ffce2f78a313
janimaenpaa <itsmayd@gmail.com>
2020-04-06 14:24:51 +0300

    Done 4.6 - 4.7, refactor tests

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,9 +1,11 @@
-const dummy = blogs => {
+const lodash = require("lodash")
+
+const dummy = (blogs) => {
   console.log(blogs)
   return 1
 }
 
-const totalLikes = blogs => {
+const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
   }
@@ -11,19 +13,51 @@ const totalLikes = blogs => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
-const favoriteBlog = blogs => {
+const favoriteBlog = (blogs) => {
   const blogWithMostLikes = blogs.reduce((previous, current) =>
     previous.likes > current.likes ? previous : current
   )
   return {
     title: blogWithMostLikes.title,
     author: blogWithMostLikes.author,
-    likes: blogWithMostLikes.likes
+    likes: blogWithMostLikes.likes,
   }
+}
+
+const mostBlogs = (blogs) => {
+  const authors = lodash.map(blogs, "author")
+  const author = lodash
+    .chain(authors)
+    .countBy()
+    .toPairs()
+    .max(lodash.last)
+    .head()
+    .value()
+  const authorWithMostBlogs = blogs.find((blog) => blog.author === author)
+
+  return authorWithMostBlogs.author
+}
+
+const mostLikes = (blogs) => {
+  const authors = lodash.uniq(lodash.map(blogs, "author"))
+  const likes = {}
+
+  for (let i = 0; i < authors.length; i++) {
+    likes[authors[i]] = 0
+  }
+  for (let i = 0; i < blogs.length; i++) {
+    likes[blogs[i].author] += blogs[i].likes
+  }
+
+  return lodash.maxBy(lodash.keys(likes), function (o) {
+    return likes[o]
+  })
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
