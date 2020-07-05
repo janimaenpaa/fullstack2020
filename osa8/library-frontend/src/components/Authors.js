b9507a970441a4a731b338da0f86921bfa75163c
janimaenpaa <itsmayd@gmail.com>
2020-07-05 03:14:44 +0300

    Done 8.8, 8.9

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,11 +1,29 @@
-  
-import React from 'react'
+import React from "react"
+import { gql, useQuery } from "@apollo/client"
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
 
 const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const authors = result.data.allAuthors
 
   return (
     <div>
@@ -14,23 +32,18 @@ const Authors = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
+          {authors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
     </div>
   )
 }
--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react"
+import { gql, useQuery } from "@apollo/client"
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+      id
+    }
+  }
+`
 
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
 
   return (
     <div>
@@ -15,20 +33,16 @@ const Books = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th>author</th>
+            <th>published</th>
           </tr>
-          {books.map(a =>
+          {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
               <td>{a.published}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
