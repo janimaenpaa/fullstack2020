a874b522af489208dfe91978a004ff0360ea254a
janimaenpaa <itsmayd@gmail.com>
2020-07-09 22:44:50 +0300

    Edit 9.8

--- osa9/patientor/backend/index.ts
+++ osa9/patientor/backend/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import cors from "cors"
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/ping", (_req, res) => {
-  res.send("ok");
+  console.log("someone pinged here");
+  res.send("pong");
 });
 
 const PORT = 3001;
