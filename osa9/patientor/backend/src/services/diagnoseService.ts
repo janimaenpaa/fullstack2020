9cbcf0e7c7f949879deaf34116677ab581986649
janimaenpaa <itsmayd@gmail.com>
2020-10-20 14:54:09 +0300

    Remove Patientor

--- osa9/patientor/backend/src/services/diagnoseService.ts
+++
@@ -1,9 +0,0 @@
-import diagnoses from "../../data/diagnoses";
-
-import { Diagnose } from "../types";
-
-const getDiagnoses = (): Array<Diagnose> => {
-  return diagnoses;
-};
-
-export default { getDiagnoses };
