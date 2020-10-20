ab6efa741b69b60529d6ad82156881905a1333b4
janimaenpaa <itsmayd@gmail.com>
2020-10-21 02:44:42 +0300

    Done 9.19

--- osa9/patientor/backend/src/types.ts
+++ osa9/patientor/backend/src/types.ts
@@ -1,11 +1,8 @@
-export interface DiagnosisEntry {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
 
 export interface Patient {
   id: string;
@@ -27,4 +24,50 @@ export enum Gender {
   Male = "male",
   Female = "female",
   Other = "other",
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
 }
